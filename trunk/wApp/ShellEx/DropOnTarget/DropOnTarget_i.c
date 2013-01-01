7588a65418946fe83bf51c53906d8137774ef8b5
walfud.s@gmail.com <walfud.s@gmail.com@b8a8fc56-3b88-13c1-e4f3-b0b44c3e8060>
2013-01-01 09:00:13 +0000

    NULL -> nullptr.

--- trunk/wApp/ShellEx/DropOnTarget/DropOnTarget_i.c
+++ trunk/wApp/ShellEx/DropOnTarget/DropOnTarget_i.c
@@ -6,10 +6,10 @@
 
 
  /* File created by MIDL compiler version 8.00.0595 */
-/* at Mon Dec 31 16:33:39 2012
+/* at Tue Jan 01 16:55:57 2013
  */
 /* Compiler settings for DropOnTarget.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0595 
+    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.00.0595 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
--- trunk/wApp/ShellEx/MenuEx/MenuEx.cpp
+++ trunk/wApp/ShellEx/MenuEx/MenuEx.cpp
@@ -12,13 +12,13 @@ using namespace ATL;
 // Used to determine whether the DLL can be unloaded by OLE.
 STDAPI DllCanUnloadNow(void)
 {
-			return _AtlModule.DllCanUnloadNow();
-	}
+	return _AtlModule.DllCanUnloadNow();
+}
 
 // Returns a class factory to create an object of the requested type.
 STDAPI DllGetClassObject(_In_ REFCLSID rclsid, _In_ REFIID riid, _Outptr_ LPVOID* ppv)
 {
-		return _AtlModule.DllGetClassObject(rclsid, riid, ppv);
+	return _AtlModule.DllGetClassObject(rclsid, riid, ppv);
 }
 
 // DllRegisterServer - Adds entries to the system registry.
@@ -26,14 +26,14 @@ STDAPI DllRegisterServer(void)
 {
 	// registers object, typelib and all interfaces in typelib
 	HRESULT hr = _AtlModule.DllRegisterServer();
-		return hr;
+	return hr;
 }
 
 // DllUnregisterServer - Removes entries from the system registry.
 STDAPI DllUnregisterServer(void)
 {
 	HRESULT hr = _AtlModule.DllUnregisterServer();
-		return hr;
+	return hr;
 }
 
 // DllInstall - Adds/Removes entries to the system registry per user per machine.
@@ -42,7 +42,7 @@ STDAPI DllInstall(BOOL bInstall, _In_opt_  LPCWSTR pszCmdLine)
 	HRESULT hr = E_FAIL;
 	static const wchar_t szUserSwitch[] = L"user";
 
-	if (pszCmdLine != NULL)
+	if (pszCmdLine != nullptr)
 	{
 		if (_wcsnicmp(pszCmdLine, szUserSwitch, _countof(szUserSwitch)) == 0)
 		{
