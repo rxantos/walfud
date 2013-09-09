9273ad3c5b9ba7a5bd79724ba5db1875d60fbc0b
walfud.s@gmail.com <walfud.s@gmail.com@b8a8fc56-3b88-13c1-e4f3-b0b44c3e8060>
2013-09-09 12:10:39 +0000

    2.2. bugfixed: can NOT quit while anti-lock described in 2.0.

--- trunk/wApp/sysLock/wAntiSysLock.cpp
+++ trunk/wApp/sysLock/wAntiSysLock.cpp
@@ -11,81 +11,45 @@ namespace w
 {
 
 // AntiSysLock.
-AntiSysLock::AntiSysLock(unsigned actInterval) : m_keeper(), m_stat(Stat::DefaultLock), m_pause()
-{
-	m_pause.lock();
-
-	m_keeper = async(&AntiSysLock::Keeper, this, shared_ptr<KeeperParam>(new KeeperParam(m_stat, m_pause, actInterval)));
-}
+AntiSysLock::AntiSysLock()
+{}
 AntiSysLock::~AntiSysLock()
 {
-	switch (m_stat)
+	m_antiLock = false;
+	if (m_keeper.valid())
 	{
-	case Stat::DefaultLock:
-		m_stat = Stat::Quit;
-		m_pause.unlock();
-		break;
-	case Stat::AntiLock:
-		break;
-	case Stat::Quit:
-		break;
-	default:
-		break;
+		m_keeper.wait();
 	}
-
-	m_keeper.wait();
 }
 
 // Interface.
 void AntiSysLock::KeepWorking()
 {
-	Stat old = m_stat;
-
-	m_stat = Stat::AntiLock;
-
-	if (old != Stat::AntiLock)
+	if (!m_antiLock)
 	{
-		m_pause.unlock();
+		m_keeper = async(&AntiSysLock::Keeper, this, nullptr);
 	}
 }
 void AntiSysLock::StopKeeping()
 {
-	Stat old = m_stat;
-
-	m_stat = Stat::DefaultLock;
-
-	if (old != Stat::DefaultLock)
-	{
-		m_pause.lock();
-	}
+	m_antiLock = false;
 }
 
 // private.
-void AntiSysLock::Keeper(shared_ptr<KeeperParam> param)
+void AntiSysLock::Keeper(void *param)
 {
-	bool running = true;
-	while (running)
+	m_antiLock = true;
+
+	while (m_antiLock)
 	{
-		param->pause.lock();
-
-		switch (param->stat)
-		{
-		case Stat::DefaultLock:
-			SetThreadExecutionState(ES_CONTINUOUS);
-			break;
-		case Stat::AntiLock:
-			SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
-			break;
-		case Stat::Quit:
-			running = false;
-			break;
-		default:
-			break;
-		}
-
-		param->pause.unlock();
-		this_thread::sleep_for(chrono::milliseconds(param->interval));
+		SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
+		this_thread::sleep_for(chrono::seconds(1));
 	}
+
+	// Restore normal default lock.
+	SetThreadExecutionState(ES_CONTINUOUS);
+
+	m_antiLock = false;
 }
 
 }
