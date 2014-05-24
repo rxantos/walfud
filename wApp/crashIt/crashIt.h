3b84386f083a1915d325352f12f5ecfe2e9b85f6
walfud.s <walfud.s@gmail.com>
2014-05-24 12:15:02 +0000

    1. rename crachIt => crashIt. 2. Add refresh function before inject by PID or ProcName. Easy to use.

---
+++ wApp/crashIt/crashIt.h
@@ -0,0 +1,1 @@
+// crachIt.h
--- wApp/crashIt/resource.h
+++ wApp/crashIt/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by crachIt.rc
+// Used by crashIt.rc
 //
 #define IDR_MAINFRAME                   128
 #define IDD_MAINDLG                     129
