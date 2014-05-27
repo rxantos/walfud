4dd10a5ff92cfb5c82bef859c129c8413a8588b6
walfud.s <walfud.s@gmail.com>
2014-05-27 06:15:10 +0000

    +Easy get/set string, integer, and raw data from registry.

--- wCode/src/wReg.h
+++ wCode/src/wReg.h
@@ -11,9 +11,24 @@
 namespace w
 {
 
+// String.
 std::string getRegSz(HKEY hKey, const std::string &subKey, const std::string &value);
 bool setRegSz(HKEY hKey, const std::string &subKey, const std::string &value, const std::string &data);
 
+std::string getRegExpandSz(HKEY hKey, const std::string &subKey, const std::string &value);
+bool setRegExpandSz(HKEY hKey, const std::string &subKey, const std::string &value, const std::string &data);
+
+std::vector<std::string> getRegMultiSz(HKEY hKey, const std::string &subKey, const std::string &value);
+bool setRegMultiSz(HKEY hKey, const std::string &subKey, const std::string &value, const std::vector<std::string> &data);
+
+// Integer.
+unsigned int getRegDw(HKEY hKey, const std::string &subKey, const std::string &value);
+bool setRegDw(HKEY hKey, const std::string &subKey, const std::string &value, unsigned int data);
+
+unsigned long long getRegQw(HKEY hKey, const std::string &subKey, const std::string &value);
+bool setRegQw(HKEY hKey, const std::string &subKey, const std::string &value, unsigned long long data);
+
+// Raw.
 bool getRegData(HKEY hKey, const std::string &subKey, const std::string &value,
 				DWORD *type, void *data, IN OUT int *len);
 bool setRegData(HKEY hKey, const std::string &subKey, const std::string &value,
