fcfcd9d314aeec63e1c9a68341bc7e2eeeb0b2d2
walfud.s@gmail.com <walfud.s@gmail.com@b8a8fc56-3b88-13c1-e4f3-b0b44c3e8060>
2014-05-27 06:15:10 +0000

    +Easy get/set string, integer, and raw data from registry.

--- trunk/wCode/src/wReg.cpp
+++ trunk/wCode/src/wReg.cpp
@@ -9,6 +9,7 @@ using namespace std;
 namespace w
 {
 
+// String.
 string getRegSz(HKEY hKey, const string &subKey, const string &value)
 {
 	string res;
@@ -25,14 +26,65 @@ string getRegSz(HKEY hKey, const string &subKey, const string &value)
 
 	return res;
 }
-bool setRegSz(HKEY hKey, const string &subKey, const string &value, const std::string &data)
+bool setRegSz(HKEY hKey, const string &subKey, const string &value, const string &data)
 {
 	bool res = setRegData(hKey, subKey, value, REG_SZ, (const BYTE *)data.c_str(), data.length());
 
 	return res;
 }
 
-// 
+string getRegExpandSz(HKEY hKey, const string &subKey, const string &value)
+{
+	string res = getRegSz(hKey, subKey, value);
+	res = expandEvnString(res);
+
+	return res;
+}
+bool setRegExpandSz(HKEY hKey, const string &subKey, const string &value, const string &data)
+{
+	bool res = setRegData(hKey, subKey, value, REG_EXPAND_SZ, (const BYTE *)data.c_str(), data.length());
+
+	return res;
+}
+
+// Integer.
+unsigned int getRegDw(HKEY hKey, const string &subKey, const string &value)
+{
+	DWORD res = 0;
+
+	int len = sizeof(res);
+	getRegData(hKey, subKey, value, nullptr, &res, &len);
+
+	return res;
+}
+bool setRegDw(HKEY hKey, const string &subKey, const string &value, unsigned int data)
+{
+	bool res = false;
+
+	setRegData(hKey, subKey, value, REG_DWORD, &data, sizeof(data));
+
+	return res;
+}
+
+unsigned long long getRegQw(HKEY hKey, const string &subKey, const string &value)
+{
+	unsigned long long res = 0;
+
+	int len = sizeof(res);
+	getRegData(hKey, subKey, value, nullptr, &res, &len);
+
+	return res;
+}
+bool setRegQw(HKEY hKey, const string &subKey, const string &value, unsigned long long data)
+{
+	bool res = false;
+
+	setRegData(hKey, subKey, value, REG_QWORD, &data, sizeof(data));
+
+	return res;
+}
+
+// Raw.
 bool getRegData(HKEY hKey, const string &subKey, const string &value,
 				DWORD *type, void *data, IN OUT int *len)
 {
